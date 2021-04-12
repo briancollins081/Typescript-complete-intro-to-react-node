795a0760f777c592fe1e65c40534d7c4cc5fe1b2
Brian Andere <briancollins081@gmail.com>
2021-04-12 11:12:08 +0300

    Types: function types

--- 01_basic_datatypes/2.types/app.js
+++ 01_basic_datatypes/2.types/app.js
@@ -5,4 +5,16 @@ function add(n1, n2) {
 function printResult(no) {
     console.log("result: " + no);
 }
+function addAndHandle(n1, n2, cb) {
+    var result = n1 + n2;
+    cb(result);
+}
 printResult(add(3400, 899));
+var combinedValues;
+combinedValues = add;
+// combinedValues = 9;
+// combinedValues = printResult;
+console.log(combinedValues(8, 8));
+addAndHandle(5, 70, function (res) {
+    console.log(res);
+});
--- 01_basic_datatypes/2.types/app.ts
+++ 01_basic_datatypes/2.types/app.ts
@@ -7,4 +7,21 @@ function printResult(no: number): void {
   console.log("result: " + no);
 }
 
+function addAndHandle(n1: number, n2: number, cb: (n: number) => void) {
+  const result = n1 + n2;
+  cb(result);
+}
+
 printResult(add(3400, 899));
+
+let combinedValues: (a: number, b: number) => number;
+
+combinedValues = add;
+// combinedValues = 9;
+// combinedValues = printResult;
+
+console.log(combinedValues(8, 8));
+
+addAndHandle(5, 70, (res) => {
+  console.log(res);
+});
