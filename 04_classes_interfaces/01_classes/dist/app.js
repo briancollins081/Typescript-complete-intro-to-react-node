ef79e1ed2364ed07f7007c96df76d9ef9993cebc
Brian Andere <briancollins081@gmail.com>
2021-04-14 10:45:09 +0300

    Classes:inheritance in js classes

--- 04_classes_interfaces/01_classes/dist/app.js
+++ 04_classes_interfaces/01_classes/dist/app.js
@@ -1,11 +1,27 @@
 "use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var Department = (function () {
-    function Department(n) {
+    function Department(id, name) {
+        this.id = id;
+        this.name = name;
         this.employees = [];
-        this.name = n;
     }
     Department.prototype.describe = function () {
-        console.log("Department: " + this.name);
+        console.log("Department: " + " " + this.id + " " + this.name);
     };
     Department.prototype.addEmployee = function (employee) {
         this.employees.push(employee);
@@ -16,9 +32,34 @@ var Department = (function () {
     };
     return Department;
 }());
-var accounting = new Department("Accounting");
-accounting.addEmployee("Prickter");
-accounting.addEmployee("Loittieffty");
-accounting.describe();
-accounting.printEmployeeInformation();
+var ITDepartment = (function (_super) {
+    __extends(ITDepartment, _super);
+    function ITDepartment(id, admins) {
+        var _this = _super.call(this, id, "IT") || this;
+        _this.admins = admins;
+        return _this;
+    }
+    return ITDepartment;
+}(Department));
+var AccountingDepartment = (function (_super) {
+    __extends(AccountingDepartment, _super);
+    function AccountingDepartment(id, reports) {
+        var _this = _super.call(this, id, "Accounting") || this;
+        _this.reports = reports;
+        return _this;
+    }
+    AccountingDepartment.prototype.addReports = function (text) {
+        this.reports.push(text);
+    };
+    AccountingDepartment.prototype.printReports = function () {
+        console.log(this.reports);
+    };
+    return AccountingDepartment;
+}(Department));
+var it = new ITDepartment("d_it_1", ["Blue", "White", "Red"]);
+console.log(it);
+var ac = new AccountingDepartment("d-ac_1", []);
+ac.addReports("What the hell happened here...");
+ac.printReports();
+console.log(ac);
 //# sourceMappingURL=app.js.map
--- 04_classes_interfaces/01_classes/src/app.ts
+++ 04_classes_interfaces/01_classes/src/app.ts
@@ -1,41 +1,73 @@
 class Department {
-  name: string;
-  private employees:string[] = [];
+  // private readonly id: string;
+  // private name: string;
+  private employees: string[] = [];
 
-  constructor(n: string) {
-    this.name = n;
+  // Define argument with access modifier to create the param as a property of the class
+  constructor(private readonly id: string, public name: string) {
+    // this.id = ID;
+    // this.name = nm;
   }
 
   /* describe() {
     console.log("Department: " + this.name);
   } */
 
-  describe(this:Department) { //type safety
-    console.log("Department: " + this.name);
-  } 
+  describe(this: Department) {
+    //type safety
+    console.log("Department: " + " " + this.id + " " + this.name);
+  }
 
-  addEmployee(employee:string){
+  addEmployee(employee: string) {
+    // this.id = "900"; //read only can not be modified after intialization - ts feature
     this.employees.push(employee);
   }
 
-  printEmployeeInformation(){
+  printEmployeeInformation() {
     console.log(this.employees.length);
     console.log(this.employees);
-    
-    
   }
 }
 
-const accounting = new Department("Accounting");
-accounting.addEmployee("Prickter");
-accounting.addEmployee("Loittieffty");
+class ITDepartment extends Department {
+  constructor(id: string, public admins: string[]) {
+    super(id, "IT");
+  }
+}
+
+class AccountingDepartment extends Department {
+  private reports: string[];
+  constructor(id: string, reports: string[]) {
+    super(id, "Accounting");
+    this.reports = reports;
+  }
+
+  addReports(text: string) {
+    this.reports.push(text);
+  }
+
+  printReports() {
+    console.log(this.reports);
+  }
+}
+
+// const accounting = new Department("d_ac_1", "Accounting");
+// accounting.addEmployee("Prickter");
+// accounting.addEmployee("Loittieffty");
 // accounting.employees[4] = "Sidney"; //not possible if it is a private attribute
 // console.log(accounting);
-accounting.describe();
-accounting.printEmployeeInformation();
-
+// accounting.describe();
+// accounting.printEmployeeInformation();
 /* const accoutingCopy = { describe: accounting.describe };
 accoutingCopy.describe(); */
 
 // const accoutingCopy = { name:"Poo", describe: accounting.describe };
 // accoutingCopy.describe();
+
+const it = new ITDepartment("d_it_1", ["Blue", "White", "Red"]);
+console.log(it);
+
+const ac = new AccountingDepartment("d-ac_1", []);
+ac.addReports("What the hell happened here...");
+ac.printReports();
+console.log(ac);
