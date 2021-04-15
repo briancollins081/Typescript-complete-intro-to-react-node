7d609b77c360eee16547736327fee8f16fab41f0
Brian Andere <briancollins081@gmail.com>
2021-04-15 07:05:51 +0300

    Interfaces:creating interfaces

---
+++ 04_classes_interfaces/01_classes/dist/classes.js
@@ -0,0 +1,100 @@
+"use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var Department = (function () {
+    function Department(id, name) {
+        this.id = id;
+        this.name = name;
+        this.employees = [];
+    }
+    Department.prototype.addEmployee = function (employee) {
+        this.employees.push(employee);
+    };
+    Department.prototype.printEmployeeInformation = function () {
+        console.log(this.employees.length);
+        console.log(this.employees);
+    };
+    Department.createEmployee = function (name) {
+        return { name: name };
+    };
+    Department.fiscalYear = 2021;
+    return Department;
+}());
+var ITDepartment = (function (_super) {
+    __extends(ITDepartment, _super);
+    function ITDepartment(id, admins) {
+        var _this = _super.call(this, id, "IT") || this;
+        _this.admins = admins;
+        return _this;
+    }
+    ITDepartment.prototype.describe = function () {
+        console.log("IT Department ID: " + this.id);
+    };
+    return ITDepartment;
+}(Department));
+var AccountingDepartment = (function (_super) {
+    __extends(AccountingDepartment, _super);
+    function AccountingDepartment(id, reports) {
+        var _this = _super.call(this, id, "Accounting") || this;
+        _this.reports = reports;
+        _this.lastReport = reports[0];
+        return _this;
+    }
+    Object.defineProperty(AccountingDepartment.prototype, "mostRecentReport", {
+        get: function () {
+            if (this.lastReport) {
+                return this.lastReport;
+            }
+            throw new Error("No report found!");
+        },
+        set: function (value) {
+            if (!value) {
+                throw new Error("Please provide a valid value for a report!");
+            }
+            this.addReports(value);
+        },
+        enumerable: false,
+        configurable: true
+    });
+    AccountingDepartment.getInstance = function () {
+        if (AccountingDepartment.instance) {
+            return this.instance;
+        }
+        this.instance = new AccountingDepartment("d2", []);
+        return this.instance;
+    };
+    AccountingDepartment.prototype.addEmployee = function (name) {
+        if (name === "ABC") {
+            return;
+        }
+        this.employees.push(name);
+    };
+    AccountingDepartment.prototype.addReports = function (text) {
+        this.reports.push(text);
+        this.lastReport = text;
+    };
+    AccountingDepartment.prototype.printReports = function () {
+        console.log(this.reports);
+    };
+    AccountingDepartment.prototype.describe = function () {
+        console.log("Accounting Department ID: " + this.id);
+    };
+    return AccountingDepartment;
+}(Department));
+var ac = AccountingDepartment.getInstance();
+var ac2 = AccountingDepartment.getInstance();
+console.log(ac, ac2);
+//# sourceMappingURL=classes.js.map
--- 04_classes_interfaces/01_classes/src/app.ts
+++ 04_classes_interfaces/01_classes/src/app.ts
@@ -1,147 +1,18 @@
-abstract class Department {
-  static fiscalYear: number = 2021;
-  // private readonly id: string;
-  // private name: string;
-  // private employees: string[] = [];
-  protected employees: string[] = []; //make it availabe in child classes
+interface Person {
+  name: string;
+  age: number;
 
-  // Define argument with access modifier to create the param as a property of the class
-  constructor(protected readonly id: string, public name: string) {
-    // this.id = ID;
-    // this.name = nm;
-  }
-
-  /* describe() {
-    console.log("Department: " + this.name);
-  } */
-
-  /* describe(this: Department) {
-    //type safety
-    console.log("Department: " + " " + this.id + " " + this.name);
-  } */
-
-  abstract describe(this: Department): void;
-
-  addEmployee(employee: string) {
-    // this.id = "900"; //read only can not be modified after intialization - ts feature
-    this.employees.push(employee);
-  }
-
-  printEmployeeInformation() {
-    console.log(this.employees.length);
-    console.log(this.employees);
-  }
-
-  // Static methods
-  static createEmployee(name: string) {
-    return { name };
-  }
+  greet(phrase: string): void;
 }
 
-class ITDepartment extends Department {
-  constructor(id: string, public admins: string[]) {
-    super(id, "IT");
-  }
-  describe() {
-    console.log("IT Department ID: " + this.id);
-  }
-}
+let user1: Person;
 
-class AccountingDepartment extends Department {
-  private reports: string[];
-  private lastReport: string;
-  private static instance: AccountingDepartment;
-  get mostRecentReport() {
-    if (this.lastReport) {
-      return this.lastReport;
-    }
-    throw new Error("No report found!");
-  }
-  set mostRecentReport(value: string) {
-    if (!value) {
-      throw new Error("Please provide a valid value for a report!");
-    }
-    this.addReports(value);
-  }
-  /* constructor(id: string, reports: string[]) {
-    super(id, "Accounting");
-    this.reports = reports;
-    this.lastReport = reports[0];
-  } */
-  // PRIVATE CONSTRUCTOR
-  private constructor(id: string, reports: string[]) {
-    super(id, "Accounting");
-    this.reports = reports;
-    this.lastReport = reports[0];
-  }
+user1 = {
+  name: "Justine",
+  age: 89,
+  greet(phrase: string) {
+    console.log(phrase + " " + this.name);
+  },
+};
 
-  static getInstance() {
-    if (AccountingDepartment.instance) {
-      return this.instance;
-    }
-    this.instance = new AccountingDepartment("d2", []);
-    return this.instance;
-  }
-
-  addEmployee(name: string) {
-    if (name === "ABC") {
-      return;
-    }
-    this.employees.push(name);
-  }
-  addReports(text: string) {
-    this.reports.push(text);
-    this.lastReport = text;
-  }
-
-  printReports() {
-    console.log(this.reports);
-  }
-
-  describe() {
-    console.log("Accounting Department ID: " + this.id);
-  }
-}
-
-// // const accounting = new Department("d_ac_1", "Accounting");
-// // accounting.addEmployee("Prickter");
-// // accounting.addEmployee("Loittieffty");
-// // accounting.employees[4] = "Sidney"; //not possible if it is a private attribute
-// // console.log(accounting);
-// // accounting.describe();
-// // accounting.printEmployeeInformation();
-// /* const accoutingCopy = { describe: accounting.describe };
-// accoutingCopy.describe(); */
-
-// // const accoutingCopy = { name:"Poo", describe: accounting.describe };
-// // accoutingCopy.describe();
-
-// const employee1 = Department.createEmployee("Andere");
-
-// console.log(employee1, Department.fiscalYear);
-
-// const it = new ITDepartment("d_it_1", ["Blue", "White", "Red"]);
-// // console.log(it);
-// it.describe();
-
-// const ac = new AccountingDepartment("d-ac_1", []);
-// // console.log(ac.mostRecentReport); // access getter as a value not a method
-// // console.log(ac);
-// ac.addReports("What the hell happened here...");
-// // console.log(ac.mostRecentReport); // access getter as a value not a method
-
-// // Using setters
-// ac.mostRecentReport = "Nano tech failure #56";
-// // console.log(ac.mostRecentReport);
-
-// // ac.printReports();
-
-// ac.addEmployee("Hellen");
-// ac.addEmployee("ABC");
-// // ac.printEmployeeInformation();
-// ac.describe();
-
-// Private constructors
-const ac = AccountingDepartment.getInstance();
-const ac2 = AccountingDepartment.getInstance();
-console.log(ac, ac2);
+user1.greet("Hello there");
