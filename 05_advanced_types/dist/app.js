baf9f385eb86c1dc22d0b81037bef19acee75a7e
Brian Andere <briancollins081@gmail.com>
2021-04-15 08:02:18 +0300

    TypeGuards:finished on type guards on classes, literal types and union types

--- 05_advanced_types/dist/app.js
+++ 05_advanced_types/dist/app.js
@@ -8,4 +8,49 @@ var e1 = {
 };
 var input;
 input = 900;
+function add(a, b) {
+    if (typeof a === "string" || typeof b === "string") {
+        return a.toString() + b.toString();
+    }
+    return a + b;
+}
+function printEmployeeInformation(emp) {
+    console.log("Name: ", emp.name);
+    if ("privillages" in emp) {
+        console.log("Privillages: ", emp.privillages);
+    }
+    if ("startDate" in emp) {
+        console.log("Start Date: ", emp.startDate);
+    }
+}
+printEmployeeInformation(e1);
+var Car = (function () {
+    function Car() {
+    }
+    Car.prototype.drive = function () {
+        console.log("Driving Car...");
+    };
+    return Car;
+}());
+var Truck = (function () {
+    function Truck() {
+    }
+    Truck.prototype.drive = function () {
+        console.log("Drving Truck");
+    };
+    Truck.prototype.loadCargo = function (amount) {
+        console.log("Loading cargo " + amount);
+    };
+    return Truck;
+}());
+var v1 = new Car();
+var v2 = new Truck();
+function useVehicle(vehicle) {
+    vehicle.drive();
+    if (vehicle instanceof Truck) {
+        vehicle.loadCargo(8000);
+    }
+}
+useVehicle(v1);
+useVehicle(v2);
 //# sourceMappingURL=app.js.map
--- 05_advanced_types/src/app.ts
+++ 05_advanced_types/src/app.ts
@@ -16,8 +16,8 @@ type Employee = { name: string; startDate: Date };
 type ElavatedEmployee = Admin & Employee; // takes all unique properties of the two
 
 // interface ElavatedEmployee extends Employee, Admin {}
-let y:ElavatedEmployee;
-y={name:"", privillages:[""], startDate:new Date()}
+let y: ElavatedEmployee;
+y = { name: "", privillages: [""], startDate: new Date() };
 const e1: ElavatedEmployee = {
   name: "Eliud",
   privillages: ["create-server"],
@@ -31,6 +31,60 @@ type Numeric = number | boolean;
 
 type Universal = Combinable & Numeric; // takes the intersection for union types
 
-let input:Universal;
+let input: Universal;
 input = 900; //intersetion is number
 
+function add(a: Combinable, b: Combinable) {
+  if (typeof a === "string" || typeof b === "string") {
+    return a.toString() + b.toString();
+  }
+  return a + b;
+}
+
+type UnknownEmployee = Employee | Admin;
+
+function printEmployeeInformation(emp: UnknownEmployee) {
+  console.log("Name: ", emp.name);
+  if ("privillages" in emp) {
+    console.log("Privillages: ", emp.privillages);
+  }
+  if ("startDate" in emp) {
+    console.log("Start Date: ", emp.startDate);
+  }
+}
+
+printEmployeeInformation(e1);
+
+// Type guards on classes
+class Car {
+  drive() {
+    console.log("Driving Car...");
+  }
+}
+
+class Truck {
+  drive() {
+    console.log("Drving Truck");
+  }
+
+  loadCargo(amount: number) {
+    console.log("Loading cargo " + amount);
+  }
+}
+type Vehicle = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+function useVehicle(vehicle: Vehicle) {
+  vehicle.drive();
+  // if ("loadCargo" in vehicle) {
+  //   vehicle.loadCargo(8000);
+  // }
+  if (vehicle instanceof Truck) {
+    vehicle.loadCargo(8000);
+  }
+}
+
+useVehicle(v1);
+useVehicle(v2);
