a12abf0ab31ad45ae69c037606999ff608580b3e
Brian Andere <briancollins081@gmail.com>
2021-04-16 11:25:20 +0300

    Decorators:adding validators with decorators

--- 07_decorators/src/app.ts
+++ 07_decorators/src/app.ts
@@ -130,7 +130,7 @@ function Autobind(
 
 class Printer {
   message: string = "This works";
-  
+
   @Autobind
   showMessage() {
     console.log(this.message);
@@ -143,3 +143,76 @@ const button = document.querySelector("button")!;
 // button.addEventListener("click", p.showMessage.bind(p));
 button.addEventListener("click", p.showMessage);
 // achieve the above using decorators
+
+// Validation with Decorators
+interface ValidatorConfig {
+  [property: string]: {
+    [validatableProp: string]: string[]; // ['required', 'positive'],
+  };
+}
+const registeredValidators: ValidatorConfig = {};
+
+function Required(target: any, propName: string) {
+  registeredValidators[target.constructor.name] = {
+    ...registeredValidators[target.constructor.name],
+    [propName]: ["required"],
+  };
+}
+
+function PositiveNumber(target: any, propName: string) {
+  registeredValidators[target.constructor.name] = {
+    ...registeredValidators[target.constructor.name],
+    [propName]: ["positive"],
+  };
+}
+
+function validate(obj: any) {
+  const objValidatorsConfig = registeredValidators[obj.constructor.name];
+  if (!objValidatorsConfig) {
+    return true;
+  }
+  let isValid = true;
+  for (const prop in objValidatorsConfig) {
+    console.log(prop);
+
+    for (const validator of objValidatorsConfig[prop]) {
+      switch (validator) {
+        case "required":
+          isValid = isValid && !!obj[prop];
+          break;
+        case "positive":
+          isValid = isValid && obj[prop] > 0;
+          break;
+      }
+    }
+  }
+  return isValid;
+}
+
+class Course {
+  @Required
+  title: string;
+  @PositiveNumber
+  price: number;
+  constructor(t: string, p: number) {
+    this.title = t;
+    this.price = p;
+  }
+}
+
+const courseForm = document.querySelector("form")!;
+courseForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const titleEl = document.getElementById("title") as HTMLInputElement;
+  const priceEl = document.getElementById("price") as HTMLInputElement;
+
+  const title = titleEl.value;
+  const price = +priceEl.value;
+
+  const createdCourse = new Course(title, price);
+  if (!validate(createdCourse)) {
+    alert("Invalid input, please try again!");
+    return;
+  }
+  console.log({ createdCourse });
+});
