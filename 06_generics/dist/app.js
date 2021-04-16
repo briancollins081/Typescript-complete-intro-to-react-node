2342baa8c692d61e7c80169782b3ae17507f0041
Brian Andere <briancollins081@gmail.com>
2021-04-16 05:29:40 +0300

    Generics: creating generics type on a function

--- 06_generics/dist/app.js
+++ 06_generics/dist/app.js
@@ -1,2 +1,7 @@
 "use strict";
+function mergeObj(objA, objB) {
+    return Object.assign(objA, objB);
+}
+var newObj = mergeObj({ name: "Andere" }, { age: 70 });
+console.log(newObj);
 //# sourceMappingURL=app.js.map
--- 06_generics/src/app.ts
+++ 06_generics/src/app.ts
@@ -7,3 +7,12 @@ const promise:Promise<string> = new Promise((resolve, reject) => {
     }, 2000);
 });
  */
+
+// Custom Generic types
+
+function mergeObj<T, U>(objA: T, objB: U) {
+  return Object.assign(objA, objB);
+}
+// console.log(mergeObj({ name: "Andere" }, { age: 70 }));
+const newObj = mergeObj({ name: "Andere" }, { age: 70 });
+console.log(newObj);
