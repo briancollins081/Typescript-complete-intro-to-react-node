f2c4a933efd9f0b8e5a9106b2a323995d022ba1c
Brian Andere <briancollins081@gmail.com>
2021-04-16 05:40:53 +0300

    Generics: contraining generic types

--- 06_generics/dist/app.js
+++ 06_generics/dist/app.js
@@ -4,4 +4,16 @@ function mergeObj(objA, objB) {
 }
 var newObj1 = mergeObj({ name: "Andere" }, { age: 70 });
 console.log(newObj1);
+function countAndDescribe(element) {
+    var descriptionText = "Got no value!";
+    if (element.length === 1) {
+        descriptionText = "Got 1 elements!";
+    }
+    else if (element.length > 1) {
+        descriptionText = "Got " + element.length + " elements!";
+    }
+    return [element, descriptionText];
+}
+console.log(countAndDescribe("Hi there!"));
+console.log(countAndDescribe([]));
 //# sourceMappingURL=app.js.map
--- 06_generics/src/app.ts
+++ 06_generics/src/app.ts
@@ -17,3 +17,19 @@ function mergeObj<T extends object, U extends object>(objA: T, objB: U) {
 const newObj1 = mergeObj({ name: "Andere" }, { age: 70 });
 // const newObj2 = mergeObj({ name: "Andere" }, 50); // after adding extends it throws
 console.log(newObj1);
+
+interface Lengthy {
+  length: number;
+}
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+  let descriptionText = "Got no value!";
+  if (element.length === 1) {
+    descriptionText = "Got 1 elements!";
+  } else if (element.length > 1) {
+    descriptionText = "Got " + element.length + " elements!";
+  }
+  return [element, descriptionText];
+}
+
+console.log(countAndDescribe("Hi there!"));
+console.log(countAndDescribe([]));
