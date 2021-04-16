0b615dd9eaba2ba7400d79ac18a1c959a5b475f6
Brian Andere <briancollins081@gmail.com>
2021-04-16 05:59:40 +0300

    Generics: generic classes

--- 06_generics/dist/app.js
+++ 06_generics/dist/app.js
@@ -1,4 +1,9 @@
 "use strict";
+var __spreadArray = (this && this.__spreadArray) || function (to, from) {
+    for (var i = 0, il = from.length, j = to.length; i < il; i++, j++)
+        to[j] = from[i];
+    return to;
+};
 function mergeObj(objA, objB) {
     return Object.assign(objA, objB);
 }
@@ -19,4 +24,31 @@ console.log(countAndDescribe([]));
 function extractAndConvert(obj, key) {
     return obj[key];
 }
+var DataStorage = (function () {
+    function DataStorage() {
+        this.data = [];
+    }
+    DataStorage.prototype.addItem = function (item) {
+        this.data.push(item);
+    };
+    DataStorage.prototype.removeItem = function (item) {
+        this.data.splice(this.data.indexOf(item), 1);
+    };
+    DataStorage.prototype.getItem = function (index) {
+        return this.data[index];
+    };
+    DataStorage.prototype.getItems = function () {
+        return __spreadArray([], this.data);
+    };
+    return DataStorage;
+}());
+var textStorage = new DataStorage();
+textStorage.addItem("Minio");
+textStorage.addItem("Madny");
+console.log(textStorage.getItems());
+var numberStorage = new DataStorage();
+numberStorage.addItem(900);
+numberStorage.addItem(800);
+console.log(numberStorage.getItems());
+console.log(numberStorage.getItem(0));
 //# sourceMappingURL=app.js.map
--- 06_generics/src/app.ts
+++ 06_generics/src/app.ts
@@ -41,3 +41,37 @@ function extractAndConvert<T extends object, U extends keyof T>(
 ) {
   return obj[key];
 }
+
+// Generic Classes
+class DataStorage<T extends string | number | boolean> {
+  private data: T[] = [];
+  addItem(item: T) {
+    this.data.push(item);
+  }
+  removeItem(item: T) {
+    this.data.splice(this.data.indexOf(item), 1);
+  }
+  getItem(index: number) {
+    return this.data[index];
+  }
+  getItems() {
+    return [...this.data];
+  }
+}
+
+const textStorage = new DataStorage<string>();
+textStorage.addItem("Minio");
+textStorage.addItem("Madny");
+console.log(textStorage.getItems());
+
+const numberStorage = new DataStorage<number>();
+numberStorage.addItem(900);
+numberStorage.addItem(800);
+console.log(numberStorage.getItems());
+console.log(numberStorage.getItem(0));
+
+// const objectStorage = new DataStorage<object>();
+// objectStorage.addItem({ name: "Hillary" });
+// objectStorage.addItem({ name: "Jeffry" });
+// // objectStorage.removeItem({name:'Jeffry'});
+// console.log(objectStorage.getItems());
