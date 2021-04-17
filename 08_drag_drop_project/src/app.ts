6ad73171a9d36a40447211b5f4ef3dc9db3238de
Brian Andere <briancollins081@gmail.com>
2021-04-17 22:55:47 +0300

    Drag&Drop:using getters to retrieve data for persons

--- 08_drag_drop_project/src/app.ts
+++ 08_drag_drop_project/src/app.ts
@@ -157,6 +157,13 @@ class ProjectListItem {
   }
 }*/
 class ProjectListItem extends Component<HTMLUListElement, HTMLLIElement> {
+  get persons() {
+    if (this.project.people === 1) {
+      return "1 person";
+    } else {
+      return `${this.project.people} persons`;
+    }
+  }
   constructor(hostId: string, private project: Project) {
     super("single-project", hostId, false, project.id);
 
@@ -166,9 +173,7 @@ class ProjectListItem extends Component<HTMLUListElement, HTMLLIElement> {
   configure() {}
   renderContent() {
     this.element.querySelector("h2")!.textContent = this.project.title;
-    this.element.querySelector(
-      "h3"
-    )!.textContent = this.project.people.toString();
+    this.element.querySelector("h3")!.textContent = this.persons + " assigned";
     this.element.querySelector("p")!.textContent = this.project.description;
   }
 }
