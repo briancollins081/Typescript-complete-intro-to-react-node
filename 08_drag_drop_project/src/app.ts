6c70bc5d8433ab8782104ad8fc8bbe896b2d661d
Brian Andere <briancollins081@gmail.com>
2021-04-17 10:42:55 +0300

    Drag&Drop:rendering project lists

--- 08_drag_drop_project/src/app.ts
+++ 08_drag_drop_project/src/app.ts
@@ -45,6 +45,41 @@ function AutoBind(
   };
   return adjustedMethod;
 }
+// Project List Class
+class ProjectList {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  listElement: HTMLElement;
+
+  constructor(private type: "active" | "finished") {
+    this.templateElement = document.getElementById(
+      "project-list"
+    )! as HTMLTemplateElement;
+    this.hostElement = document.getElementById("app")! as HTMLDivElement;
+
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true
+    );
+
+    this.listElement = importedNode.firstElementChild as HTMLFormElement;
+    this.listElement.id = `${this.type}-projects`;
+
+    this.attach();
+    this.renderContent();
+  }
+
+  private renderContent() {
+    const listId = `${this.type}-projects-list`;
+    this.listElement.querySelector("ul")!.id = listId;
+    this.listElement.querySelector("h2")!.textContent =
+      this.type.toUpperCase() + " PROJECTS";
+  }
+
+  private attach() {
+    this.hostElement.insertAdjacentElement("beforeend", this.listElement);
+  }
+}
 
 // Project Input
 class ProjectInput {
@@ -142,3 +177,5 @@ class ProjectInput {
 }
 
 const projectInput = new ProjectInput();
+const activePrjList = new ProjectList("active");
+const finishedPrjList = new ProjectList("finished");
