38005a0ca1c9fe9f0f78e35c6cddf2ad2853f86f
Brian Andere <briancollins081@gmail.com>
2021-04-18 20:57:45 +0300

    Typescript:libs:using ts libs that work both in Typescript and Vanilla.js == class-transformer

--- 11_third_party_modules/01/src/app.ts
+++ 11_third_party_modules/01/src/app.ts
@@ -1,7 +1,27 @@
-import _ from "lodash";
+//1. USING VANILLA JS BASED LIBS
+
+/* import _ from "lodash";
 declare var GLOBAL: any;
 // Installed lodash types to use it in ts
 console.log(_.shuffle([1, 8, 20]));
 
 // libs or code without types i.e.
-console.log(GLOBAL);
+console.log(GLOBAL); */
+
+// 2. USING TYPESCRIPT BASED LIBS THAT WORK BOTH IN VANILLA JS AND TYPESCRIPT
+/* import "reflect-metadata";
+import { plainToClass } from "class-transformer";
+
+import { Product } from "./product.model";
+const products = [
+  { title: "A Carpet", price: 23.5 },
+  { title: "A Keyholder", price: 10.49 },
+];
+// const loadedProducts = products.map((p) => new Product(p.title, p.price));
+const loadedProducts = plainToClass(Product, products);
+// const p1 = new Product("A book", 33.59);
+loadedProducts.forEach((p) => {
+  console.log(p.getInformation());
+});
+ */
+// 3. USING TYPESCRIPT ONLY LIBS
