a0cc75aa11e3ad2aa8db2b33475f41e2fd116331
Brian Andere <briancollins081@gmail.com>
2021-04-18 19:44:46 +0300

    Webpack:Typescript:setting up basic dev and pro configuration

--- 10_ts_webpack/webpack.config.prod.js
+++ 10_ts_webpack/webpack.config.prod.js
@@ -1,20 +1,14 @@
 const path = require("path");
+const CleanPlugin = require("clean-webpack-plugin");
 
 module.exports = {
-  mode:"development",
+  mode: "production",
   entry: "./src/app.ts",
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
-    publicPath: "dist",
   },
-  devServer: {
-    contentBase: path.join(__dirname),
-    compress: true,
-    publicPath: "dist",
-    port: 9000,
-  },
-  devtool: "inline-source-map",
+//   devtool: "inline-source-map",
   module: {
     rules: [
       {
@@ -27,4 +21,6 @@ module.exports = {
   resolve: {
     extensions: [".ts", ".js"],
   },
+
+  plugins: [new CleanPlugin.CleanWebpackPlugin()],
 };
