963b2bd2e190124c9efd44069ad4c2e23ba2f765
Brian Andere <briancollins081@gmail.com>
2021-04-18 20:25:58 +0300

    Typescript:declare:Using declare as a last resort where no @types exist

--- 11_third_party_modules/01/src/app.ts
+++ 11_third_party_modules/01/src/app.ts
@@ -1,3 +1,7 @@
 import _ from "lodash";
-
+declare var GLOBAL: any;
+// Installed lodash types to use it in ts
 console.log(_.shuffle([1, 8, 20]));
+
+// libs or code without types i.e.
+console.log(GLOBAL);
